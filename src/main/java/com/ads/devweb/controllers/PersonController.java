d3161abaf5e514d607c2543876760708174dc757
Anisberto Reis <anisbertoos@gmail.com>
2021-02-09 19:22:56 -0300

    Controller endpoints

--- src/main/java/com/ads/devweb/controllers/PersonController.java
+++ src/main/java/com/ads/devweb/controllers/PersonController.java
@@ -19,8 +19,26 @@ public class PersonController {
         return personService.getAll();
     }
 
+    @GetMapping(value = "/{id}")
+    public Person getById(@PathVariable Long id) {
+        return personService.getPersonById(id);
+    }
+
     @PostMapping
     public Person savePerson(@RequestBody Person person) {
         return personService.savePerson(person);
+    }
+
+    @DeleteMapping(value = "/{id}/ads")
+    public String deletePerson(@PathVariable Long id) {
+        personService.deletePerson(id);
+        return "Pessoal deletada!";
+    }
+
+    @PutMapping(value = "{id}")
+    public Person updatePerson(@PathVariable Long id) {
+        personService.updatePerson(personService.getPersonById(id));
+        Person person = personService.getPersonById(id);
+        return person;
     }
 }
