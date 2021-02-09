064dabc0c739c06580dcbd6160c5aabd91a66388
Anisberto Reis <anisbertoos@gmail.com>
2021-02-09 14:05:42 -0300

    Methods was Create

--- src/main/java/com/ads/devweb/controllers/PersonController.java
+++ src/main/java/com/ads/devweb/controllers/PersonController.java
@@ -1,4 +1,26 @@
 package com.ads.devweb.controllers;
 
+import com.ads.devweb.entities.Person;
+import com.ads.devweb.services.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/persons")
 public class PersonController {
+
+    @Autowired
+    private PersonService personService;
+
+    @GetMapping
+    public List<Person> getAll() {
+        return personService.getAll();
+    }
+
+    @PostMapping
+    public Person savePerson(@RequestBody Person person) {
+        return personService.savePerson(person);
+    }
 }
--- src/main/java/com/ads/devweb/entities/Person.java
+++ src/main/java/com/ads/devweb/entities/Person.java
@@ -1,4 +1,74 @@
 package com.ads.devweb.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
 public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+
+    public Person() {
+
+    }
+
+    public Person(Long id, String nome, String email, String telefone) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(id, person.id) && Objects.equals(nome, person.nome) && Objects.equals(email, person.email) && Objects.equals(telefone, person.telefone);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome, email, telefone);
+    }
 }
